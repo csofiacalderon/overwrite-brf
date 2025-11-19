27d47015985e055dc72384630e0a8d82da963998
Carla Calderon <carlacalderon@curative.com>
2025-11-19 11:11:21 -0600

    making changes to folder location paths

--- brf_calculation.py
+++ brf_calculation.py
@@ -1,7 +1,3 @@
-
-
-
-
 def calculate_group_brf(plans, claims_probability_distribution, deductible_threshold_data, coinsurance_threshold_data, moop_threshold_data, pcp_copay_data, sps_copay_data, er_copay_data):
     total_group_enrollment = 0
     weighted_group_brf = 0
